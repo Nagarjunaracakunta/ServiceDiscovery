0b7d007b9b0b1b59e29155e49f4fb5cf20cab08f
Nagarjuna Rachakunta <nagarjunar@Nagarjunas-MacBook-Pro.local>
2026-09-19 20:55:16 +0530

    added comment

--- src/main/java/com/example/servicediscovery/ServicediscoveryApplication.java
+++ src/main/java/com/example/servicediscovery/ServicediscoveryApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class ServicediscoveryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Eureka Server...");
 		SpringApplication.run(ServicediscoveryApplication.class, args);
 	}
 
